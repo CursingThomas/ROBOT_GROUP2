0cf2a4befe43b60dae92854bd2c410a493abf876
Thomas Schenk <thschenk143@gmail.com>
2022-04-03 21:56:42 +0200

    MQTT almost done

--- Code/POC's & Examples/MQTT_POC_ROBOT/lib/ArduinoOTA-1.0.8/examples/Advanced/OTASketchDownloadWifi/arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-// Insert your WiFi secrets here:
-#define SECRET_SSID "" // Your network SSID (name)    
-#define SECRET_PASS "" // Your network password
--- Code/POC's & Examples/MQTT_POC_ROBOT/src/main.cpp
+++ Code/POC's & Examples/MQTT_POC_ROBOT/src/main.cpp
@@ -1,67 +1,35 @@
+#include "EspMQTTClient.h"
 #include <Arduino.h>
-#include "PubSubClient.h"  
-#include "ArduinoJson.h"
-#include <WiFi.h>
-#include <ArduinoOTA.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
 
-const char* ssid = "Internet";
-const char* password = "TLE9009tle";
+EspMQTTClient client(
+  "Internet",
+  "TLE9009tle",
+  "test.mosquitto.org", // MQTT Broker
+  "ThomasESP",     // Client name that uniquely identify your device
+  1883              // The MQTT port, default to 1883. this line can be omitted
+);
 
-const char* mqtt_server = "192.168.2.254";
-void setup_wifi();
-void callback(char* topic, byte* message, unsigned int length);
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
+void onConnectionEstablished()
+{
+  client.subscribe("Test/mqtt", [](const String & payload) {
+    Serial.println(payload);
+  });
+  client.publish("Test/mqtt", "This is a message from client1");
+}
 
 
-void setup() 
+void setup()
 {
   Serial.begin(115200);
-
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-
-}
-
-void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
+  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+  client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overrited with enableHTTPWebUpdater("user", "password").
+  client.enableLastWillMessage("Test/mqtt", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
   
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
 }
 
-void loop() 
+void loop()
 {
+  client.loop();
+  client.publish("Test/mqtt", "Message sent from esp");
+  delay(1000);
 }
