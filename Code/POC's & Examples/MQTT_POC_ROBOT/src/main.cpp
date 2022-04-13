617292339c5c079135ad9bcab8641d08e11b2d75
Schenk,Thomas T.J.W <t.schenk@student.fontys.nl>
2022-04-13 20:44:40 +0200

    added some stuff to mqtt

--- Code/POC's & Examples/MQTT_POC_ROBOT/src/main.cpp
+++ Code/POC's & Examples/MQTT_POC_ROBOT/src/main.cpp
@@ -1,20 +1,21 @@
 #include "EspMQTTClient.h"
+#include <PubSubClient.h>
 #include <Arduino.h>
 
 EspMQTTClient client(
-  "Internet",
-  "TLE9009tle",
-  "test.mosquitto.org", // MQTT Broker
+  "12connect",
+  "",
+  "mqtt.eclipseprojects.io", // MQTT Broker
   "ThomasESP",     // Client name that uniquely identify your device
   1883              // The MQTT port, default to 1883. this line can be omitted
 );
 
 void onConnectionEstablished()
 {
-  client.subscribe("Test/mqtt", [](const String & payload) {
+  client.subscribe("BIPDemo/Messages", [](const String & payload) {
     Serial.println(payload);
   });
-  client.publish("Test/mqtt", "This is a message from client1");
+  client.publish("BIPDemo/Messages", "I am connected!");
 }
 
 
@@ -23,13 +24,14 @@ void setup()
   Serial.begin(115200);
   client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
   client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overrited with enableHTTPWebUpdater("user", "password").
-  client.enableLastWillMessage("Test/mqtt", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+  client.enableLastWillMessage("BIPDemo/Messages", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+  
   
 }
 
 void loop()
 {
   client.loop();
-  client.publish("Test/mqttESP", "Message sent from esp");
-  delay(5000);
+  //client.publish("BIPDemo/Ack", "Message sent from esp");
+  //delay(5000);
 }
