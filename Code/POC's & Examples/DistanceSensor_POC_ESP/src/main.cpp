03a265377512deda2e3ca6d60ba3ffaf1928c1aa
Thomas Schenk <thschenk143@gmail.com>
2022-05-08 18:21:06 +0200

    Added a distancesensor POC

---
+++ Code/POC's & Examples/DistanceSensor_POC_ESP/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#define echoPin 2
+#define trigPin 3
+
+
+long duration;
+int distance;
+
+void setup() 
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+void loop() 
+{
+  int prevDistance = distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  if (distance < (prevDistance - 10))
+  {
+    Serial.println("Something just passed");
+  }
+}
